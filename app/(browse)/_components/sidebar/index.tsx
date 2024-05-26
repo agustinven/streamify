2a625291868c3d038234f41317d9796b5269971b
Agustin Avendaño <agusavendano94@gmail.com>
2024-05-26 20:01:17 -0300

    The functionality corresponding to the sidebar section is created within the web page. Added external dependencies react-tooltip from shadcn, usehooks-ts, zustand. The corresponding folder system is created to achieve this functionality. A store folder is created to manage the global state of the application between components.

---
+++ app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,13 @@
+import Toogle from "./toggle"
+import { Wrapper } from "./wrapper"
+
+
+const Sidebar = () => {
+  return (
+    <Wrapper>
+      <Toogle/>
+    </Wrapper>
+  )
+}
+
+export default Sidebar
